902e764021b65dcacc6bb127ba316c049de0a7e5
Kefentse-Mfoloe <kefentse.mfoloe@gmail.com>
2026-07-08 22:58:02 +0200

    Add building blocks + models + endpoints

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,40 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price) : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler(IDocumentSession session)
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // Create a new product based on the command object
+            var product = new Product()
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            // Save the product to the database
+            session.Store(product);
+            session.SaveChangesAsync(cancellationToken);
+
+            // Return the result with the new product's ID
+
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
